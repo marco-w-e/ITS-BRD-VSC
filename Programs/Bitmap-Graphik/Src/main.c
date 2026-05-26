477a0c767ff255efd0b6c810e7e249168a12651e
marco-w-e <Marco.Weidner@haw-hamburg.de>
2026-05-26 14:27:59 +0200

    lala

--- Programs/Bitmap-Graphik/Src/main.c
+++ Programs/Bitmap-Graphik/Src/main.c
@@ -15,20 +15,35 @@
 #include "fontsFLASH.h"
 #include "additionalFonts.h"
 #include "error.h"
+#include "input.h"
 
 
 int main(void) {
-	initITSboard();    // Initialisierung des ITS Boards
+	initITSboard(); // Initialisierung des ITS Boards
+	initInput();   
+	openNextFile();
 	
 	GUI_init(DEFAULT_BRIGHTNESS);   // Initialisierung des LCD Boards mit Touch
 	TP_Init(false);                 // Initialisierung des LCD Boards mit Touch
 
   // Begruessungstext	
 	lcdPrintlnS("Hallo liebes TI-Labor (c-project)");
-	
+	int x = 1;
+	int y = 1;
+
 	// Test in Endlosschleife
 	while(1) {
-		HAL_Delay(10000);
+		
+		while (x != 400){ 
+			Coordinate corrdinat;
+			corrdinat.x = x;
+			corrdinat.y = y;
+			GUI_drawPoint(corrdinat, 35000,DOT_PIXEL_1X1,DOT_FILL_AROUND );
+			x++ ;
+
+		}
+		x = 0;
+		y++;
 	}
 }
 
